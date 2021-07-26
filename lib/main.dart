57cdc04bb4ce6ff0cc927cdf70a5894a0c44cf86
Sandeep <sandeep.bangia2012@gmail.com>
2021-07-26 15:31:46 +0530

    First commit Message

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext contex) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          //body: ProductManager('Food Tester'),
+      ),
+    );
+  }
+}
